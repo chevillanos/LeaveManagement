b691112832387e15103162f60fc40c2f362aa360
Carl Henry Villanos <carlevillanos@live.com>
2020-11-02 02:44:00 +0800

    Add project files.

---
+++ LeaveManagement/Mappings/DTOMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using LeaveManagement.Data;
+using LeaveManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Mappings
+{
+    public class DTOMapper : Profile
+    {
+        public DTOMapper()
+        {
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeCreateVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagement/Models/LeaveTypeVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Models
+{
+    public class LeaveTypeVM
+    {        
+        public int Id { get; set; }        
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
